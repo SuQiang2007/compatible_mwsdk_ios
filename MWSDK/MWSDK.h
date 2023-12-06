c178c6e7b15d3d1a6b8b5886747c5aac1610a05c
Squall <2573040560@qq.com>
2023-12-06 19:03:27 +0800

    Initial Commit

---
+++ MWSDK/MWSDK.h
@@ -0,0 +1,18 @@
+//
+//  MWSDK.h
+//  MWSDK
+//
+//  Created by squall on 2023/12/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MWSDK.
+FOUNDATION_EXPORT double MWSDKVersionNumber;
+
+//! Project version string for MWSDK.
+FOUNDATION_EXPORT const unsigned char MWSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWSDK/PublicHeader.h>
+
+
