b43a2a91dcafca5c06b1e1b90053f7415e71a64c
Squall <2573040560@qq.com>
2023-12-06 23:51:41 +0800

    first commit

--- MWSDK/MWSDK.h
+++ MWSDK/MWSDK.h
@@ -15,4 +15,11 @@ FOUNDATION_EXPORT const unsigned char MWSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MWSDK/PublicHeader.h>
 
+@interface MWSDK : NSObject
 
+/**
+ Logs a message to the console.
+ */
++ (void)logMessage;
+
+@end
---
+++ MWSDK/MWSDK.m
@@ -0,0 +1,11 @@
+// MWSDK.m
+
+#import "MWSDK.h"
+
+@implementation MWSDK
+
++ (void)logMessage {
+    NSLog(@"Logging a message from MWSDK.");
+}
+
+@end
